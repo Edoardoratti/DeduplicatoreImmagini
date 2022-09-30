31ebceb526618569095bfe255287c1243aaa5978
edoardo.ratti <edoardo.ratti@423-07.CPT.local>
2022-09-30 14:39:19 +0200

    Inizislizzazione codice

--- 5_Applicativo/Deduplicatore/src/deduplicatore/Deduplicatore.java
+++ 5_Applicativo/Deduplicatore/src/deduplicatore/Deduplicatore.java
@@ -1,20 +1,80 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package deduplicatore;
 
-/**
- *
- * @author edoardo.ratti
- */
-public class Deduplicatore {
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.*;
+import org.opencv.core.*;
+import org.bytedeco.opencv.opencv_core.*;
+import org.bytedeco.opencv.opencv_imgproc.*;
 
-    /**
-     * @param args the command line arguments
-     */
+
+/*
+ * @author Edoardo Ratti
+ * @version
+ *
+ * Classe conentente i codici sorgente per controllare la similitudine
+ * tra immagini.
+ */
+
+public class Deduplicatore {
+    static{ System.loadLibrary(Core.NATIVE_LIBRARY_NAME); }
+    IplImage[] images;
+    static final String[] EXTENSIONS = new String[]{
+        "jpg", "png", "webp"
+    };
+    
+    static final FilenameFilter IMAGE_FILTER = new FilenameFilter() {
+
+        @Override
+        public boolean accept(final File dir, final String name) {
+            for (final String ext : EXTENSIONS) {
+                if (name.endsWith("." + ext)) {
+                    return (true);
+                }
+            }
+            return (false);
+        }
+    };
+    
+    public void getEveryImages(String path){
+        File root = new File(path);
+        File[] list = root.listFiles();
+
+        if (list == null) return;
+
+        for ( File f : list ) {
+            if ( f.isDirectory() ) {
+                getEveryImages( f.getAbsolutePath() );
+                System.out.println( "Dir:" + f.getAbsoluteFile() );
+            }
+            else {// controllo estensione
+                System.out.println( "File:" + f.getAbsoluteFile() );
+                ////ACTION
+            }
+        }
+        //return images;
+    }
+    
+    public void searchForDirectory(){
+        
+    }
+    
+    public void compareImage(){
+        
+    }
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        Path directoryPath;
+        
+        try{
+            directoryPath = Paths.get(args[0]);
+            byte[] b = Files.readAllBytes(directoryPath);
+            System.out.println(b);
+        }catch(IOException e){
+            throw new IllegalArgumentException("Errore durante la lettura del file");
+        }
     }
     
 }
